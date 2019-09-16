7586195ce432bbd4ec72ae7fc1a29d42cc287f77
Guillermo Latumahina <glatumahina@roc-dev.com>
2019-09-16 09:18:23 +0200

    Uitwerking van de exercise

---
+++ rijbewijs.php
@@ -0,0 +1,14 @@
+<?php
+
+echo "Hoe oud ben je? \n";
+
+$leeftijd = readline();
+
+if ($leeftijd > 16.5) {
+	echo "Je mag beginnnen met rijlesen!";
+	# code...
+} else {
+	echo "Helaas, je mag nog niet beginnen met rijlessen";
+	# code...
+}
+?>
